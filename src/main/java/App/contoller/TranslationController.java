94c051228c0ded9fc25279d24d172e304ef78dfd
Brendow271 <pashashkuropat@yandex.ru>
2024-08-04 21:03:09 +0300

    Added an exception handler
    - Created TranslationException
    - Update dependencies
    - Added error handler in TranslationController
    - Added error handler in TranslationService
    - Added request limiter controller

--- src/main/java/App/contoller/TranslationController.java
+++ src/main/java/App/contoller/TranslationController.java
@@ -1,7 +1,9 @@
 package App.contoller;
 
+import App.exception.TranslationException;
 import App.service.TranslationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,11 +21,18 @@ public class TranslationController {
     }
 
     @PostMapping
-    public String translate(@RequestBody Map<String, String> request, HttpServletRequest httpServletRequest) {
+    public ResponseEntity<String> translate(@RequestBody Map<String, String> request, HttpServletRequest httpServletRequest) {
         String text = request.get("texts");
         String sourceLanguageCode = request.get("sourceLanguageCode");
         String targetLanguageCode = request.get("targetLanguageCode");
         String userIp = httpServletRequest.getRemoteAddr();
-        return translationService.translateText(text, sourceLanguageCode, targetLanguageCode);//, userIp
+
+        try {
+            String translatedText = translationService.translateText(text, sourceLanguageCode, targetLanguageCode);
+            return ResponseEntity.ok(translatedText);
+        } catch (TranslationException e) {
+            return new ResponseEntity<>(e.getMessage(), e.getStatus());
+        }
+//        return translationService.translateText(text, sourceLanguageCode, targetLanguageCode);//, userIp
     }
 }
--- src/main/java/App/service/TranslationService.java
+++ src/main/java/App/service/TranslationService.java
@@ -1,18 +1,17 @@
 package App.service;
 
+import App.exception.TranslationException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 import java.util.stream.Collectors;
 import java.io.IOException;
 
@@ -23,6 +22,9 @@ public class TranslationService {
     private final String idFolder;
     private final ExecutorService executorService;
     private final ObjectMapper objectMapper;
+    private final Semaphore semaphore;
+    private final ScheduledExecutorService scheduler;
+
     @Autowired
     public TranslationService(RestTemplate restTemplate) {
         this.yandexApiKey = "t1.9euelZrOmJTHxsaQzcuUx8iMm4yLje3rnpWanseNl5LNz5zKkZyWnZidkczl9fdkQEJK-e9JGsTd9fckbz9K-e9JGsTN5_XrnpWaz5yayJGNzMfIl4-TjJvGzpXv_MXrnpWaz5yayJGNzMfIl4-TjJvGzpU.sC57OEUuuCUp86Alvn5yfg_eUkWzSo2zZRbVKXdL0T1MjxW28FOJvFrjRpGuUSAzNvNLsnjIpySb_lKkMKsGBQ";
@@ -30,20 +32,36 @@ public class TranslationService {
         this.idFolder = "b1g9l2de1r3n3ivlesju";
         this.executorService = Executors.newFixedThreadPool(10);
         this.objectMapper = new ObjectMapper();
+        this.semaphore = new Semaphore(20);
+        this.scheduler = Executors.newScheduledThreadPool(1);
     }
 
     public String translateText(String text, String sourceLanguageCode, String targetLanguageCode) {//, String userIp
 
         List<Future<String>> futures = List.of(text.split("\\s+")).stream()
-                .map(word -> executorService.submit(() -> translateWord(word, sourceLanguageCode, targetLanguageCode)))
+                .map(word -> executorService.submit(() -> {
+                    try {
+                        semaphore.acquire();
+                        return translateWord(word, sourceLanguageCode, targetLanguageCode);
+                    } finally {
+                        scheduler.schedule(() -> semaphore.release(), 1, TimeUnit.SECONDS);
+                    }
+                }))
                 .collect(Collectors.toList());
 
+
         StringBuilder translatedText = new StringBuilder();
+
         for (Future<String> future : futures) {
             try {
                 translatedText.append(future.get()).append(" ");
             } catch (InterruptedException | ExecutionException e) {
-                e.printStackTrace();
+                if (e.getCause() instanceof TranslationException) {
+                    throw (TranslationException) e.getCause();
+                } else {
+                    e.printStackTrace();
+                    translatedText.append("Error ");
+                }
             }
         }
 
@@ -65,21 +83,38 @@ public class TranslationService {
 
         HttpEntity<Map<String, String>> entity = new HttpEntity<>(body, headers);
 
-        ResponseEntity<String> response = restTemplate.exchange(apiUrl, HttpMethod.POST, entity, String.class);
-        return parseTranslationResponse(response.getBody());
+        try {
+            ResponseEntity<String> response = restTemplate.exchange(apiUrl, HttpMethod.POST, entity, String.class);
+            if (response.getStatusCode() == HttpStatus.OK) {
+
+                return parseTranslationResponse(response.getBody());
+            } else {
+                throw new TranslationException("Error: Received non-200 response code.", (HttpStatus) response.getStatusCode());
+            }
+        } catch (HttpClientErrorException.BadRequest e) {
+            throw new TranslationException("Error: Invalid language code or bad request.", HttpStatus.BAD_REQUEST);
+        } catch (HttpClientErrorException.Unauthorized e) {
+            throw new TranslationException("Error: Unauthorized. Check your API key.", HttpStatus.UNAUTHORIZED);
+        } catch (Exception e) {
+            System.out.println(e);
+            throw new TranslationException("Error: An unexpected error occurred.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+//        ResponseEntity<String> response = restTemplate.exchange(apiUrl, HttpMethod.POST, entity, String.class);
+//        return parseTranslationResponse(response.getBody());
     }
 
     private String parseTranslationResponse(String response) {
-
         try {
             JsonNode root = objectMapper.readTree(response);
             JsonNode translationsNode = root.path("translations");
             if (translationsNode.isArray() && translationsNode.size() > 0) {
                 return translationsNode.get(0).path("text").asText();
+            } else {
+                throw new TranslationException("Error: No translations found in the response.", HttpStatus.INTERNAL_SERVER_ERROR);
             }
         } catch (IOException e) {
             e.printStackTrace();
+            throw new TranslationException("Error: Failed to parse the translation response.", HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return "";
     }
 }
